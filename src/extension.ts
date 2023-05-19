6d5ac91cb154aa617520e9d45e91a04c34f8731b
kazuwombat <matsumotokazuya7@gmail.com>
2023-05-19 20:46:13 +0900

    InitialCommit

---
+++ src/extension.ts
@@ -0,0 +1,80 @@
+// src/extension.ts
+import * as vscode from "vscode"
+import * as fs from "fs"
+import * as path from "path"
+
+export function activate(context: vscode.ExtensionContext) {
+  const configPath = path.join(vscode.workspace.rootPath!, ".typo")
+
+  if (!fs.existsSync(configPath)) {
+    vscode.window.showInformationMessage(
+      ".typo file does not exist in your workspace. Please create one to enable typo fixing."
+    )
+    // Display a message box with the option to create a .typo file.
+    vscode.window
+      .showInformationMessage(
+        ".typo file does not exist in your workspace. Please create one to enable typo fixing.",
+        "Create .typo file"
+      )
+      .then((option) => {
+        if (option === "Create .typo file") {
+          fs.writeFileSync(configPath, JSON.stringify({}), "utf8")
+        }
+      })
+  }
+
+  const fixTypo = vscode.workspace.onDidSaveTextDocument(
+    async (document: vscode.TextDocument) => {
+      const activeEditor = vscode.window.activeTextEditor
+
+      if (
+        !activeEditor ||
+        document.uri.toString() !== activeEditor.document.uri.toString() ||
+        path.basename(document.uri.fsPath) === ".typo"
+      ) {
+        return
+      }
+
+      try {
+        const config = JSON.parse(fs.readFileSync(configPath, "utf-8"))
+        const workspaceEdit = await calcWorkspaceEdit(document, config)
+
+        // Apply the workspace edit
+        await vscode.workspace.applyEdit(workspaceEdit)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  )
+  context.subscriptions.push(fixTypo)
+}
+
+async function calcWorkspaceEdit(
+  document: vscode.TextDocument,
+  config: { [key: string]: string[] }
+) {
+  const workspaceEdit = new vscode.WorkspaceEdit()
+
+  for (const correctWord in config) {
+    const typos = config[correctWord]
+    typos.forEach((typo) => {
+      const regex = new RegExp(`\\b${typo}\\b`, "g")
+
+      let match
+      while ((match = regex.exec(document.getText()))) {
+        const startPos = document.positionAt(match.index)
+        const endPos = document.positionAt(match.index + typo.length)
+
+        workspaceEdit.replace(
+          document.uri,
+          new vscode.Range(startPos, endPos),
+          correctWord
+        )
+      }
+    })
+  }
+
+  return workspaceEdit
+}
+
+export function deactivate() {}
